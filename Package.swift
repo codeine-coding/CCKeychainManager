1badb132516cebbe61eb4ba86473b7adc4255f4a
Allen Whearry <allen@codeinecoding.com>
2019-08-18 02:12:13 -0400

    update swift tools version

--- Package.swift
+++ Package.swift
@@ -1,13 +1,10 @@
-// swift-tools-version:5.0
+// swift-tools-version:4.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "CCKeychainManager",
-    platforms: [
-        .iOS(.v11)
-    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
