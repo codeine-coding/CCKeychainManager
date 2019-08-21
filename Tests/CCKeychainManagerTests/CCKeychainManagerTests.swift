0b1044e2afed36a3732705551c751ba87ae47c34
Allen Whearry <allen@codeinecoding.com>
2019-08-21 13:45:37 -0400

    initial manager tests

--- Tests/CCKeychainManagerTests/CCKeychainManagerTests.swift
+++ Tests/CCKeychainManagerTests/CCKeychainManagerTests.swift
@@ -2,5 +2,22 @@ import XCTest
 @testable import CCKeychainManager
 
 final class CCKeychainManagerTests: XCTestCase {
-
+    var accessConfig: KeychainItemConfig!
+    var refreshConfig: KeychainItemConfig!
+    
+    var accessItem: KeychainItem!
+    var refreshItem: KeychainItem!
+    
+    override func setUp() {
+        accessConfig = KeychainItemConfig(keyLabelFor: .accessToken, server: "testServer")
+        refreshConfig = KeychainItemConfig(keyLabelFor: .refreshToken, server: "testServer")
+        
+        accessItem = KeychainItem(config: accessConfig)
+        refreshItem = KeychainItem(config: refreshConfig)
+    }
+    
+    override func tearDown() {
+        accessItem = nil
+        refreshItem = nil
+    }
 }
